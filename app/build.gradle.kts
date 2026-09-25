90e5fa767657dfbf900744eda67c8ca2c736b100
baccours <74301348+baccours@users.noreply.github.com>
2026-09-25 15:13:46 +0200

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,11 +9,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.baccours.nyx"
+        applicationId = "com.baccours.nyxx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
